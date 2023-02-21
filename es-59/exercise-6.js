3b8a4ea50992935e43be5613a2d14ee67d74d4d3
Raccoon6650 <mario.caruso098@gmail.com>
2023-02-21 11:22:36 +0100

    Esercizio completo

---
+++ es-59/exercise-6.js
@@ -0,0 +1,41 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate(value){
+    if(value instanceof Square) {
+      return value.side * value.side
+    }
+    else if(value instanceof Circle) {
+      return value.radius*2*Math.PI
+    }
+
+    else if(value instanceof Rectangle) {
+      return value.width * value.height
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
