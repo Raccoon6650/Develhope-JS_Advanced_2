765bf74857bbc78d69f45a75bfad2ef66d6e08e2
Raccoon6650 <mario.caruso098@gmail.com>
2023-03-01 11:32:00 +0100

    .

--- es-75/exercise.js
+++ es-75/exercise.js
@@ -1,1 +1,7 @@
+function repeatHello (callback, i){
+    const index = setInterval(callback, 1000);
+    setTimeout(() => clearInterval(index), 1000 * i)
+}
 
+
+repeatHello(() => {console.log("Arrow hello!") }, 5);
