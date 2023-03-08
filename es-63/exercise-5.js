b1b9d7e53af1ea2f7806b72e02a69f8cf452b112
Raccoon6650 <mario.caruso098@gmail.com>
2023-03-08 09:53:04 +0100

    Esercizio

--- es-63/exercise-5.js
+++ es-63/exercise-5.js
@@ -6,5 +6,5 @@ const person = {
 };
 
 
-const { id, name = person.firstName, surname = person.lastName, old = person.age} = person;
+const { id, firstName : name, lastName : surname, age : old } = person;
 console.log(id, name, surname, old);
