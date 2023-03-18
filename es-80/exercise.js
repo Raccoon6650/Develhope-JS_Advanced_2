03b1e8b36f37ef641096681d93b1930bc52ca6f7
Mario Caruso <120800521+Raccoon6650@users.noreply.github.com>
2023-03-18 18:43:43 +0100

    Update exercise.js

--- es-80/exercise.js
+++ es-80/exercise.js
@@ -3,3 +3,20 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function saveData(key, obj){
+  if (typeof localStorage !== "undefined") {
+    const obj_JSON = JSON.stringify(obj);
+    localStorage.setItem(key, obj_JSON);
+  } else {
+    console.log("LocalStorage non è disponibile nel tuo browser");
+  }
+}
+
+function getData(key){
+  const jsonData = localStorage.getItem(key);
+  const data = JSON.parse(jsonData);
+  return data
+}
+saveData("user", user)
+console.log(getData("user"));
